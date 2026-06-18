8018a7d42c5b1f5bcc19b0e11e4bbeca70273f6f
Alejandro Jimenez <alextresel22@gmail.com>
2026-06-18 20:15:52 +0200

    modificado para generar carpeta de logs automaticamente

--- logger.py
+++ logger.py
@@ -2,6 +2,7 @@ import logging
 import os
 import sys
 
+
 class Logger:
 
     def __init__(self):
@@ -11,15 +12,18 @@ class Logger:
 
         if self.logging.hasHandlers():
             self.logging.handlers.clear()
+
         self.logging.setLevel(logging.INFO)
 
         formatter = logging.Formatter(
-        "%(asctime)s | %(levelname)s | %(message)s"
+            "%(asctime)s | %(levelname)s | %(message)s"
         )
 
-        RUTA_LOG_FILE = os.path.join("logs", "banco.log")
-        
-        file_handler = logging.FileHandler(RUTA_LOG_FILE, encoding="utf-8")
+        os.makedirs("logs", exist_ok=True)
+        ruta_log_file = os.path.join("logs", "banco.log")
+
+        file_handler = logging.FileHandler(ruta_log_file, encoding="utf-8")
+        file_handler.setFormatter(formatter)
 
         console_handler = logging.StreamHandler(sys.stdout)
         console_handler.setFormatter(formatter)
