95acc06aadd4405e555d4449104ac7bccb279740
Alejandro Jimenez <alextresel22@gmail.com>
2026-06-18 22:19:56 +0200

    solucionado problema de test (nombre erroneo de metodos)

--- test_eventos.py
+++ test_eventos.py
@@ -6,15 +6,15 @@ class TestCalendarioLaboral(unittest.TestCase):
 
     def test_fin_de_semana_es_no_laborable(self):
         sabado = date(2026, 1, 3)  # un sábado real
-        self.assertTrue(CalendarioLaboral.es_dia_no_laborable(sabado))
+        self.assertTrue(CalendarioLaboral.es_dia_festivo(sabado))
 
     def test_dia_laborable_normal(self):
         lunes = date(2026, 1, 5)  # un lunes normal, sin festivo
-        self.assertFalse(CalendarioLaboral.es_dia_no_laborable(lunes))
+        self.assertFalse(CalendarioLaboral.es_dia_festivo(lunes))
 
     def test_festivo_es_no_laborable(self):
         navidad = date(2026, 12, 25)
-        self.assertTrue(CalendarioLaboral.es_dia_no_laborable(navidad))
+        self.assertTrue(CalendarioLaboral.es_dia_festivo(navidad))
 
 
 if __name__ == "__main__":
